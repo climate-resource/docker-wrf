a9128fe34eb1c44f3d768e84216f36873436d7df
Jared Lewis <jared@jared.kiwi.nz>
2018-03-05 17:33:03 +1300

    Tag docker images with git sha

--- build.sh
+++ build.sh
@@ -11,4 +11,4 @@ region=$1
 
 echo "Running build."
 mkdir -p logs
-packer build -var-file secrets.json -var-file $region --only amazon-ebs template.json > >(tee logs/stdout-ebs.log) 2> >(tee logs/stderr-ebs.log >&2)
+packer build -var-file secrets.json -var-file $region -var "git_sha=`git rev-parse --short HEAD`" --only amazon-ebs template.json > >(tee logs/stdout-ebs.log) 2> >(tee logs/stderr-ebs.log >&2)
