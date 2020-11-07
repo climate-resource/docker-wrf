0e87f99052052aba2b3dc2e37255d6ab6189cf18
Jared Lewis <jared@jared.kiwi.nz>
2020-11-07 21:17:19 +1100

    Update to ubuntu2004

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:latest
+FROM ubuntu:20.04
 
 MAINTAINER Jared Lewis <jared@jared.kiwi.nz>
 
--- scripts/add_augury.sh
+++ scripts/add_augury.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-# Make sure that the permissions on the ssh keys are correct
-chmod 600 ~/.ssh/id_rsa
 
-ssh-keyscan -t rsa gitlab.com >> ~/.ssh/known_hosts
-
-git clone git@gitlab.com:lewisjarednz/augury-wrf.git /opt/wrf/augury-wrf
+git clone https://${DEPLOY_USER}:${DEPLOY_PASS}@gitlab.com:lewisjarednz/augury-wrf.git /opt/wrf/augury-wrf
 
 # Setup the temporary directory which is where a EBS volume will be mounted
 sudo mkdir /mnt/temp
